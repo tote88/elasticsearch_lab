5e116a4878e6e4cfe5c7cc3be6b839b352a0c3a9
Jordi Casanovas <tote88@gmail.com>
2018-04-20 20:57:02 +0200

    adds q2

--- src/main/java/partb/Q2.java
+++ src/main/java/partb/Q2.java
@@ -1,9 +1,43 @@
 package partb;
 
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchType;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import util.Utils;
+
 public class Q2 {
 
 	public static void execute() throws Exception {
-		
+		// Get ElasticSearch client
+		TransportClient client = Utils.getClient();
+
+		// Because its case sensitive
+		SearchResponse response = client.prepareSearch("shakespeare")
+				.setSearchType(SearchType.QUERY_THEN_FETCH)
+				.setQuery(QueryBuilders.termQuery("speaker", "EARL OF WORCESTER"))	// Change this line to implement queries
+				.setSize(Utils.MAX_RESULTS)
+				.setExplain(true)
+				.get();
+
+		// Iterate through query results and print them
+		SearchHits hits = response.getHits();
+		int hitsCount = hits.getHits().length;
+		System.out.println("Query executed successfully. "
+				+ "\nQuery Time: " + response.getTookInMillis() + " ms"
+				+ "\nResults Count: " + hitsCount
+				+ "\nResults: ");
+
+		for (int i = 0; i < hitsCount; i++) {
+			SearchHit hit = hits.getAt(i);
+			System.out.println("[" + hit.getScore() + "] "
+					+ hit.getSourceAsString());
+		}
+
+		// IMPORTANT // Close ElasticSearch client
+		Utils.closeClient(client);
 	}
 
 }
