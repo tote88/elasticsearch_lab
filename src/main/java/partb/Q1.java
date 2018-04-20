149f7891b0eca20643b8a582144fc7926da10d69
Jordi Casanovas <tote88@gmail.com>
2018-04-20 20:49:13 +0200

    fix packages and set q1

---
+++ src/main/java/partb/Q1.java
@@ -0,0 +1,42 @@
+package partb;
+
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchType;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import util.Utils;
+
+public class Q1 {
+
+	public static void execute() throws Exception {
+		// Get ElasticSearch client
+		TransportClient client = Utils.getClient();
+
+		SearchResponse response = client.prepareSearch("shakespeare")
+				.setSearchType(SearchType.QUERY_THEN_FETCH)
+				.setQuery(QueryBuilders.termQuery("line_id", 2018))	// Change this line to implement queries
+				.setSize(Utils.MAX_RESULTS)
+				.setExplain(true)
+				.get();
+
+		// Iterate through query results and print them
+		SearchHits hits = response.getHits();
+		int hitsCount = hits.getHits().length;
+		System.out.println("Query executed successfully. "
+				+ "\nQuery Time: " + response.getTookInMillis() + " ms"
+				+ "\nResults Count: " + hitsCount
+				+ "\nResults: ");
+
+		for (int i = 0; i < hitsCount; i++) {
+			SearchHit hit = hits.getAt(i);
+			System.out.println("[" + hit.getScore() + "] "
+					+ hit.getSourceAsString());
+		}
+
+		// IMPORTANT // Close ElasticSearch client
+		Utils.closeClient(client);
+	}
+
+}
--- src/main/java/partc/Q1.java
+++
@@ -1,9 +0,0 @@
-package partb;
-
-public class Q1 {
-
-	public static void execute() throws Exception {
-
-	}
-
-}
