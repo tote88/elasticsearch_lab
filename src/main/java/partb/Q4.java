52faa665e4097036d0f0080bba22e0400b42cbb7
Jordi Casanovas <tote88@gmail.com>
2018-04-22 16:52:23 +0200

    add queries 3, 4 and 5 from Victor

--- src/main/java/partb/Q4.java
+++ src/main/java/partb/Q4.java
@@ -1,9 +1,48 @@
 package partb;
 
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchType;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import util.Utils;
+
 public class Q4 {
 
 	public static void execute() throws Exception {
-		
+		// Get ElasticSearch client
+		TransportClient client = Utils.getClient();
+
+		SearchResponse response = client.prepareSearch("shakespeare")
+				.setSearchType(SearchType.QUERY_THEN_FETCH)
+				.setQuery(
+						QueryBuilders.boolQuery()
+								.must(QueryBuilders.matchQuery("text_entry", "Juliet"))
+								.must(QueryBuilders.termQuery("play_name", "Romeo and Juliet"))
+								.mustNot(QueryBuilders.termQuery("speaker", "ROMEO"))
+				)	// Change this line to implement queries
+				.setSize(Utils.MAX_RESULTS)
+				.setExplain(true)
+				.get();
+
+		// Iterate through query results and print them
+		SearchHits hits = response.getHits();
+		int hitsCount = hits.getHits().length;
+		System.out.println("Query executed successfully. "
+				+ "\nQuery Time: " + response.getTookInMillis() + " ms"
+				+ "\nResults Count: " + hitsCount
+				+ "\nResults: ");
+
+		for (int i = 0; i < hitsCount; i++) {
+			SearchHit hit = hits.getAt(i);
+			System.out.println("[" + hit.getScore() + "] "
+					+ hit.getSourceAsString());
+		}
+
+		// IMPORTANT // Close ElasticSearch client
+		Utils.closeClient(client);
 	}
 
 }
--- src/main/java/partb/Q5.java
+++ src/main/java/partb/Q5.java
@@ -1,9 +1,47 @@
 package partb;
 
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchType;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+
+import util.Utils;
+
 public class Q5 {
 
 	public static void execute() throws Exception {
+		// Get ElasticSearch client
+		TransportClient client = Utils.getClient();
+
+		// Build query and execute it
+		SearchResponse response = client.prepareSearch("shakespeare")
+				.setSearchType(SearchType.QUERY_THEN_FETCH)
+				.setQuery(
+						QueryBuilders.matchPhraseQuery("text_entry", "My love")
+						)
+				.setPostFilter(QueryBuilders.termQuery("play_name", "Romeo and Juliet"))
+				.setSize(Utils.MAX_RESULTS)
+				.setExplain(true)
+				.get();
 		
+		// Iterate through query results and print them
+		SearchHits hits = response.getHits();
+		int hitsCount = hits.getHits().length;
+		System.out.println("Query executed successfully. "
+				+ "\nQuery Time: " + response.getTookInMillis() + " ms"
+				+ "\nResults Count: " + hitsCount
+				+ "\nResults: ");
+
+		for (int i = 0; i < hitsCount; i++) {
+			SearchHit hit = hits.getAt(i);
+			System.out.println("[" + hit.getScore() + "] "
+					+ hit.getSourceAsString());
+		}
+		
+		// IMPORTANT // Close ElasticSearch client
+		Utils.closeClient(client);
 	}
 
 }
